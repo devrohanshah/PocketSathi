d580967922b08a78efb2d74d6af07462bf373f39
Rohan Shah <140335546+devrohanshah@users.noreply.github.com>
2026-08-01 18:50:03 +0545

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,19 @@
-const CACHE_NAME = "pocketsathi-v5";
+const CACHE_NAME = "pocketsathi-v7";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.json",
   "./css/styles.css",
   "./js/app.js",
-  "./icons/icon.svg"
+  "./icons/icon.svg",
+  "./icons/favicon.svg",
+  "./icons/favicon.png",
+  "./icons/icon-512.png",
+  "./icons/icon-192.png",
+  "./icons/icon-96.png",
+  "./icons/apple-touch-icon.png",
+  "./icons/splash.svg",
+  "./icons/splash-640x1136.png"
 ];
 
 self.addEventListener("install", (event) => {
